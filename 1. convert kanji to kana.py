31164f9447e3613b1e0da86fc87a75d8ad9be562
name <mail@mail.mail>
2018-08-08 21:40:49 +0200

    finished converter kanji to kana

--- 1. convert kanji to kana.py
+++ 1. convert kanji to kana.py
@@ -1,13 +1,9 @@
 import libs.kanji_to_kana
 
-# with open("inputs/sentences.csv", "r", encoding="utf8") as input:
-#     with open("intermediateFiles/sentencesWithKana.csv", "w", encoding="utf8") as output:
-#         for line in input:
-#             line = line.strip()
-#             sentenceId, language, sentenceText = line.split("\t")
-
 with open("inputs/sentences.csv", "r", encoding="utf8") as input:
-    for line in input:
-        sentenceId, language, sentenceText = line.split("\t")
-        sentenceText = sentenceText.strip()
-        if language == "jpn"
+    with open("intermediateFiles/sentencesWithKana.csv", "w", encoding="utf8") as output:
+        for line in input:
+            sentenceId, language, sentenceText = line.split("\t")
+            sentenceText = sentenceText.strip()
+            sentenceKana = kanji_to_kana.kanjiSentenceToKana(sentenceText) if language == "jpn" else ""
+            output.write("%s\t%s\t%s\t%s\n" % (sentenceId, language, sentenceKana))
