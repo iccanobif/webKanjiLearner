9e23d12461c849cef562ed19118a11869e4d01ab
name <mail@mail.mail>
2018-08-11 16:06:47 +0200

    succesfully generates aggregated csv

--- 1. convert kanji to kana.py
+++ 1. convert kanji to kana.py
@@ -3,7 +3,8 @@ import libs.kanji_to_kana
 with open("inputs/sentences.csv", "r", encoding="utf8") as input:
     with open("intermediateFiles/sentencesWithKana.csv", "w", encoding="utf8") as output:
         for line in input:
-            sentenceId, language, sentenceText = line.split("\t")
-            sentenceText = sentenceText.strip()
+            sentenceId, language, sentenceText = line.strip().split("\t")
+            if language not in ["eng", "jpn"]:
+                continue
             sentenceKana = libs.kanji_to_kana.kanjiSentenceToKana(sentenceText) if language == "jpn" else ""
             output.write("%s\t%s\t%s\t%s\n" % (sentenceId, language, sentenceText, sentenceKana))
--- 2. build aggregated csv.py
+++ 2. build aggregated csv.py
@@ -1,60 +1,56 @@
-import kanji_to_kana
-
 blacklist = "︎ 　_-－—―〜・･,，、､；:：!！‼?？.．…。｡'‘’\"“”(（)）[]{}〈〉「｢」｣『』【】〔〕@*/／＼＆＃%％•※^゜°℃→∈+＋×=＝≠>~～−√∠⊂○♪⨯々ゝゞー$＄₪€0〇０1１①¹2２②²₂3３³4４5５6６7７8８9９" \
             + "AaａＡBbｂＢcCｃＣdDｄＤeEＥｅéFfＦｆgGｇＧhHＨｈIiＩｉJjｊＪkKＫｋlLｌＬMmｍＭNnＮｎOoｏＯºPpｐＰqQｑＱrRｒＲℝSsＳｓßTtＴｔuUｕＵÚvVｖＶwWｗＷXxｘＸyYＹｙzZＺｚαβ" \
             + "ぁあァアぃいィイぅうゥウヴぇえェエぉおォオかヵカがガきキぎギ㌔くクぐグ㌘けヶケげゲこコごゴさサざザしシじジ〆すスずズせセぜゼそソぞゾたタだダちチぢヂっつッツづヅてテでデとトどドなナにニ" \
             + "ぬヌねネのノはハばバぱパひヒびビぴピふフぶブぷプへヘべベぺペほホぼボぽポまマみミむムﾑめメもモゃやャヤゅゆュユょよョヨらラりリるルれレろロわワゐヸゑヱをヲんン​"
 
 class Frase:
-    def __init__(self, fraseInGiapponese, traduzioni):
+    def __init__(self, fraseInGiapponese, fraseInKana, traduzioni):
         self.fraseInGiapponese = fraseInGiapponese
         self.traduzioni = traduzioni
-        self.fraseInGiapponeseInKana = kanji_to_kana.kanjiSentenceToKana(fraseInGiapponese)
+        self.fraseInGiapponeseInKana = fraseInKana
         self.frequenzaMinima = None
 
 def preparaFrasi():
-    japaneseSentences = dict()
-    englishSentences = dict()
+    japaneseSentences = dict() # Key is a sentenceId, value is a Frase object
+    englishSentences = dict() # Key is a sentenceId, value is a string
+
     frequenzaCaratteri = dict() # La chiave è un carattere giapponese, il valore è il numero di occorrenze nel dataset
-    frasi = dict()
 
     print("Parso sentences.csv")
-    with open("sentences.csv", "r", encoding="utf8") as f:
+    with open("intermediateFiles/sentencesWithKana.csv", "r", encoding="utf8") as f:
         for line in f.readlines():
-            split = line.strip().split("\t")
-            if split[1] == "jpn":
-                japaneseSentences[split[0]] = split[2]
-                for c in split[2]:
+            sentenceId, language, sentenceText, sentenceKana = line.split("\t")
+            sentenceKana = sentenceKana.strip()
+            if language == "jpn":
+                japaneseSentences[sentenceId] = Frase(sentenceText, sentenceKana, [])
+
+                for c in sentenceText:
                     if c in blacklist:
                         continue
                     if c in frequenzaCaratteri:
                         frequenzaCaratteri[c] += 1
                     else:
                         frequenzaCaratteri[c] = 1
-            if split[1] == "eng":
-                englishSentences[split[0]] = split[2]
+
+            if language == "eng":
+                englishSentences[sentenceId] = sentenceText
 
     print("Parso links.csv")
-    with open("links.csv", "r", encoding="utf8") as links:
+    with open("inputs/links.csv", "r", encoding="utf8") as links:
         for line in links.readlines():
-            split = line.strip().split("\t")
-            if split[0] in japaneseSentences and split[1] in englishSentences:
-                fraseGiapponese = japaneseSentences[split[0]]
-                fraseInglese = englishSentences[split[1]]
-                if fraseGiapponese in frasi:
-                    frasi[fraseGiapponese].traduzioni.append(fraseInglese)
-                else:
-                    frasi[fraseGiapponese] = Frase(fraseGiapponese, [fraseInglese])
+            id1, id2 = line.strip().split("\t")
+            if id1 in japaneseSentences and id2 in englishSentences:
+                japaneseSentences[id1].traduzioni.append(englishSentences[id2])
 
     print("Calcolo frequenza carattere meno frequente per ogni frase")
-    for frase in frasi.values():
+    for frase in japaneseSentences.values():
         frase.frequenzaMinima = None
         for c in frase.fraseInGiapponese:
             if c in blacklist:
                 continue
             if frase.frequenzaMinima is None or frequenzaCaratteri[c] < frase.frequenzaMinima:
                 frase.frequenzaMinima = frequenzaCaratteri[c]
-    return frasi.values(), frequenzaCaratteri
+    return japaneseSentences.values(), frequenzaCaratteri
 
 frasi, frequenzaCaratteri = preparaFrasi()
 # Le frasi di un carattere sono tutte le frasi in cui quel carattere è il piu' difficile che hanno
@@ -64,7 +60,7 @@ def makeJson(carattere, fraseGiapponese, fraseInGiapponeseInKana, traduzione):
             % (carattere, frequenzaCaratteri[carattere], fraseGiapponese, fraseInGiapponeseInKana, traduzione)
 
 print("Preparing mapping")
-with open("frasi giapponesi 2.csv", "w", encoding="utf8") as f:
+with open("output/frasi giapponesi 2.csv", "w", encoding="utf8") as f:
     for c in sorted(frequenzaCaratteri.keys(), key=lambda x: frequenzaCaratteri[x]):
         print("Doing character", c, frequenzaCaratteri[c])
         f.write("\n".join([makeJson(c, frase.fraseInGiapponese, frase.fraseInGiapponeseInKana, traduzione) \
