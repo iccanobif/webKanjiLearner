225eaf7b1990fb03454ff4306757555404068e96
iccanobif <iccanobif@fake.mail>
2018-09-19 19:03:18 +0200

    Refactoring: move SentenceRepository class to its own file

--- web/kanjiLearning.js
+++ web/kanjiLearning.js
@@ -1,14 +1,11 @@
 const express = require("express")
 const app = express()
 const http = require("http").Server(app)
-const fs = require("fs")
-const readline = require("readline")
 const sqlite3 = require("sqlite3")
 const bodyParser = require("body-parser");
 const kanjidic = require("./kanjidic.js")
 const ut = require("./utils.js")
-
-const maxPostDataLength = 10000
+const sr = require("./sentenceRepository.js")
 
 function readCookie(cookies, cookieName)
 {
@@ -24,68 +21,7 @@ function readCookie(cookies, cookieName)
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-let SentenceRepository = function ()
-{
-    this.sentences = {}
-    this.isLoaded = false
-    this.loadSentences = () =>
-    {
-        readline
-            .createInterface({ input: fs.createReadStream("../output/allSentences.csv") })
-            .on("line", (line) =>
-            {
-                if (line.trim() == "")
-                    return;
-                // Format:
-                // {"char": "糞", "freq": 1, "jpn": "糞です", "kana": "くそです", "eng": "It's poop"}
-                parsed = JSON.parse(line)
-                if (parsed["char"] in this.sentences)
-                    this.sentences[parsed["char"]].push(parsed)
-                else
-                    this.sentences[parsed["char"]] = [parsed]
-            })
-            .on("close", () =>
-            {
-                ut.log("Finished loading datasets")
-                this.isLoaded = true
-            })
-    }
-    this.getFullListOfRandomSentences = () =>
-    {
-        // TODO: precompute sorted keys instead of sorting at every request
-        return Object.keys(this.sentences)
-            .map((c) =>
-            {
-                let index = Math.floor((this.sentences[c].length) * Math.random())
-                return this.sentences[c][index]
-            })
-            .sort((a, b) =>
-            {
-                if (a.freq > b.freq) return -1;
-                if (a.freq < b.freq) return 1;
-                return 0;
-            })
-    }
-    this.getRandomSentence = (char) =>
-    {
-        if (!(char in this.sentences))
-            return null
-
-        let index = Math.floor((this.sentences[char].length) * Math.random())
-        return this.sentences[char][index]
-    }
-    this.getAllSentences = (char) =>
-    {
-        if (!(char in this.sentences))
-            return []
-        else
-            return this.sentences[char]
-    }
-
-    this.loadSentences()
-}
-
-let sentenceRepository = new SentenceRepository()
+let sentenceRepository = new sr.SentenceRepository()
 
 let HiddenCharacterRepository = function ()
 {
---
+++ web/sentenceRepository.js
@@ -0,0 +1,64 @@
+const fs = require("fs")
+const readline = require("readline")
+const ut = require("./utils.js")
+
+module.exports.SentenceRepository = function ()
+{
+    this.sentences = {}
+    this.isLoaded = false
+    this.loadSentences = () =>
+    {
+        readline
+            .createInterface({ input: fs.createReadStream("../output/allSentences.csv") })
+            .on("line", (line) =>
+            {
+                if (line.trim() == "")
+                    return;
+                // Format:
+                // {"char": "糞", "freq": 1, "jpn": "糞です", "kana": "くそです", "eng": "It's poop"}
+                parsed = JSON.parse(line)
+                if (parsed["char"] in this.sentences)
+                    this.sentences[parsed["char"]].push(parsed)
+                else
+                    this.sentences[parsed["char"]] = [parsed]
+            })
+            .on("close", () =>
+            {
+                ut.log("Finished loading datasets")
+                this.isLoaded = true
+            })
+    }
+    this.getFullListOfRandomSentences = () =>
+    {
+        // TODO: precompute sorted keys instead of sorting at every request
+        return Object.keys(this.sentences)
+            .map((c) =>
+            {
+                let index = Math.floor((this.sentences[c].length) * Math.random())
+                return this.sentences[c][index]
+            })
+            .sort((a, b) =>
+            {
+                if (a.freq > b.freq) return -1;
+                if (a.freq < b.freq) return 1;
+                return 0;
+            })
+    }
+    this.getRandomSentence = (char) =>
+    {
+        if (!(char in this.sentences))
+            return null
+
+        let index = Math.floor((this.sentences[char].length) * Math.random())
+        return this.sentences[char][index]
+    }
+    this.getAllSentences = (char) =>
+    {
+        if (!(char in this.sentences))
+            return []
+        else
+            return this.sentences[char]
+    }
+
+    this.loadSentences()
+}
