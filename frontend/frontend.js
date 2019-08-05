a11046e8ddb983c53169717e907cec0f0d081397
iccanobif <iccanobif@420blaze.it>
2019-08-05 08:13:55 +0200

    added node.js web server for serving frontend

---
+++ frontend/frontend.js
@@ -0,0 +1,14 @@
+const PORT = 4200
+
+const express = require("express")
+const app = express()
+const http = require("http").Server(app)
+
+app.use(express.static('dist/webKanjiLearnerFrontend'))
+app.use((req, res, next) =>
+{
+    res.sendFile("index.html", { root: "dist/webKanjiLearnerFrontend" });
+})
+
+http.listen(PORT, "0.0.0.0")
+console.log("Server running on port " + PORT)
