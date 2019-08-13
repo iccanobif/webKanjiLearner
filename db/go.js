8b0847e202be519d42576819189dffb00af33bea
iccanobif <iccanobif@420blaze.it>
2019-08-14 00:42:34 +0200

    fix go.js: clear event loop from useless setInterval handles

--- db/go.js
+++ db/go.js
@@ -23,11 +23,27 @@ module.exports.go = function (elements, limit, initialGap, operation)
             }
         }
 
+        const handles = []
         for (let i = 0; i < limit; i++)
         {
-            new Promise(resolve => setTimeout(resolve, initialGap * i))
+            new Promise(resolve => 
+            {
+                handles.push(setTimeout(resolve, initialGap * i))
+            })
                 .then(() =>
                 {
+                    // Sometimes the execution of the entire task is quicker
+                    // than initialGap * limit milliseconds, so in this case
+                    // it's good to clear the event loop from setTimeout() tasks
+                    // that have become useless
+                    if (count >= elements.length)
+                    {
+                        handles.forEach(element =>
+                        {
+                            clearTimeout(element)
+                        });
+                        return;
+                    }
                     doNextOperation().catch(console.error)
                 })
         }
