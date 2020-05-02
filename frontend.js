d85767560e82d2ea56602c500379ef63e66eec64
iccanobif <iccanobif@fake.mail>
2020-05-02 22:12:24 +0200

    use heroku port

--- frontend.js
+++ frontend.js
@@ -1,15 +1,14 @@
-const PORT = 4200
+const PORT =
+  process.env.PORT == undefined ? 4200 : Number.parseInt(process.env.PORT);
 
-const express = require("express")
-const app = express()
-const http = require("http").Server(app)
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
 
-app.use(express.static('dist/webKanjiLearnerFrontend'))
-app.use((req, res, next) =>
-{
-    res.sendFile("index.html", { root: "dist/webKanjiLearnerFrontend" });
-})
+app.use(express.static("dist/webKanjiLearnerFrontend"));
+app.use((req, res, next) => {
+  res.sendFile("index.html", { root: "dist/webKanjiLearnerFrontend" });
+});
 
-http.listen(PORT, "0.0.0.0")
-console.log("Server running on port " + PORT)
-
+http.listen(PORT, "0.0.0.0");
+console.log("Server running on port " + PORT);
